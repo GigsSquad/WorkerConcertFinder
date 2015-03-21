5d4b5ad1ad2c63210e1e3213cd984df0d004b6e5
Rafal <rkpieniazek@gmail.com>
2015-03-21 15:02:27 +0100

    Zmiana insertow po rozdzieleniu bazy

--- src/main/java/com/humandevice/wrk/backend/workers/ParseWorker.java
+++ src/main/java/com/humandevice/wrk/backend/workers/ParseWorker.java
@@ -4,6 +4,8 @@ import com.humandevice.wrk.backend.others.MapMgr;
 import com.humandevice.wrk.backend.others.Normalizer;
 import com.humandevice.wrk.backend.pojos.LatLngPojo;
 import com.mysql.jdbc.MysqlDataTruncation;
+import com.mysql.jdbc.Statement;
+
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
@@ -16,7 +18,7 @@ public abstract class ParseWorker extends Worker {
 	protected PreparedStatement pstm = null;
 	protected ResultSet resultSet = null;
 	protected String agencyName;
-	private static final String table = "concerts";
+	private static final String table = "gigs";
 
 	/*
 	 * Abstract class to get data from agencies
@@ -33,13 +35,17 @@ public abstract class ParseWorker extends Worker {
 			LatLngPojo latlng = MapMgr.getCoordinates(conSpot, conCity);
 
 			//pobieram
-			String get = String.format("SELECT * FROM %s WHERE artist = ? AND city = ? AND day = ? AND month = ? AND year = ?", table);
+			String get = String.format("select * from %s" +
+                    " join spots on gigs.id_spot = spots.id_spot" +
+                    " join artists on gigs.id_artist = artists.id_artist "+
+                    "WHERE artist = ? AND city = ? AND date=?", table);
+
 			pstm = connection.prepareStatement(get);
 			pstm.setString(1, conArtist);
 			pstm.setString(2, conCity);
-			pstm.setInt(3, conDay);
-			pstm.setInt(4, conMonth);
-			pstm.setInt(5, conYear);
+			pstm.setString(3, String.format("%d-%d-%d", conYear, conMonth, conDay));
+
+          //  System.out.println()
 			resultSet = pstm.executeQuery();
 			String log;
 
@@ -52,29 +58,83 @@ public abstract class ParseWorker extends Worker {
 				log = String.format("DODAJE %-4.4s %-12.12s %-20.20s %-18.18s %-18.18s %-40.40s", agencyName, conCity, conSpot, latlng.getLatitude(),
 						latlng.getLongitude(), conArtist);
 				System.out.println(log);
+            ////////////////////nowa wersja
 
-				String insert = String
-						.format("INSERT IGNORE INTO %s(artist, city, spot, day, month, year, agency, url, lat, lon) VALUES(?,?,?,?,?,?,?,?,?,?)", table);
+                //patrzymy czy jest artysta
+                String getArt = "select id_artist from artists where artist=?";
+                pstm = connection.prepareStatement(getArt);
+                pstm.setString(1, conArtist);
+                resultSet = pstm.executeQuery();
 
-				pstm = connection.prepareStatement(insert);
-				pstm.setString(1, conArtist);
-				pstm.setString(2, conCity);
-				pstm.setString(3, conSpot);
-				pstm.setInt(4, conDay);
-				pstm.setInt(5, conMonth);
-				pstm.setInt(6, conYear);
-				pstm.setString(7, agencyName);
-				pstm.setString(8, conUrl);
-				pstm.setString(9, latlng.getLatitude());
-				pstm.setString(10, latlng.getLongitude());
-				pstm.executeUpdate();
+                int id_artist=0;
+                if (!resultSet.next())// jezeli nie bylo artysty to go dodajemy
+                {
+                    String insertToArtTable = "INSERT INTO artists(artist) VALUES(?)";
+                    pstm = connection.prepareStatement(insertToArtTable, Statement.RETURN_GENERATED_KEYS);
+                    pstm.setString(1, conArtist);
+                    pstm.executeUpdate();
 
-			}
+                    ResultSet idSet = pstm.getGeneratedKeys(); //get last inserted id
+                    idSet.next();
+                    id_artist = idSet.getInt(1);
+                    System.out.println("id_artist"+id_artist);
+                } else {
+                    id_artist = resultSet.getInt("id_artist");
+                }
+
+
+
+
+                //patrzymy czy jest lokalizacja
+                String getSpot = "select id_spot from spots where city = ? and spot=? ";
+                pstm = connection.prepareStatement(getSpot);
+                pstm.setString(1,conCity);
+                pstm.setString(2,conSpot);
+                resultSet = pstm.executeQuery();
+                int id_spot=0;
+
+                if(!resultSet.next()) { // jezeli nie ma spot to dodajemy
+                    String insertToArtTable = "INSERT IGNORE INTO spots(city,spot,lat,lon,country) VALUES(?,?,?,?,?)";
+                    pstm = connection.prepareStatement(insertToArtTable,Statement.RETURN_GENERATED_KEYS);
+                    pstm.setString(1, conCity);
+                    pstm.setString(2, conSpot);
+                    pstm.setString(3, latlng.getLatitude());
+                    pstm.setString(4, latlng.getLongitude());
+                    pstm.setString(5, "PL");
+                   pstm.executeUpdate();
+
+                        ResultSet idSet = pstm.getGeneratedKeys();//get last inserted id
+                    idSet.next();
+                    id_spot = idSet.getInt(1);
+                            System.out.println("id_spot"+id_spot);
+                }
+                else {
+                    id_spot = resultSet.getInt("id_spot");
+                }
+                // okej teraz wsadzamy wszystko razem
+
+                String insertToGigs = String
+                        .format("INSERT IGNORE INTO %s(id_artist, id_spot, date, agency, url) VALUES(?,?,DATE ?,?,?)", table);
+
+                pstm = connection.prepareStatement(insertToGigs);
+                pstm.setInt(1, id_artist);
+                pstm.setInt(2, id_spot);
+                pstm.setString(3, String.format("%d-%d-%d", conYear, conMonth, conDay));
+                pstm.setString(4,agencyName);
+                pstm.setString(5, conUrl);
+
+                pstm.executeUpdate();
+
+		}
 		} catch (MysqlDataTruncation trnce) {
 			sqlError("za długa kolumna " + conArtist);
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
+        catch (NullPointerException npe)
+        {
+            System.err.println("NULL POINTER"+conArtist); // to bedzie do wyjebania
+        }
 	}
 
 	/**
@@ -101,7 +161,7 @@ public abstract class ParseWorker extends Worker {
 	 * @throws SQLException
 	 */
 	public void updateLatLng(boolean forceUpdate) throws SQLException {
-		String select = String.format("SELECT * FROM %s", table);
+		String select = "SELECT * FROM spots";
 
 		if (!forceUpdate) {
 			select = String.format("%s WHERE lat = 0 OR lon = 0", select);
@@ -111,7 +171,7 @@ public abstract class ParseWorker extends Worker {
 		resultSet = pstm.executeQuery();
 
 		while (resultSet.next()) {
-			String update = String.format("UPDATE %s SET lat = ?, lon = ? WHERE ord = ?", table);
+			String update = "UPDATE spots SET lat = ?, lon = ? WHERE ord = ?";
 			pstm = connection.prepareStatement(update);
 
 			LatLngPojo latlon = MapMgr.getCoordinates(resultSet.getString(3), resultSet.getString(4));
